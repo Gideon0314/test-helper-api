381c07676e3746c5357ad4a0a86974c14aebcc12
Gideon0314 <923198284@qq.com>
2021-01-07 10:37:43 +0800

    V1.0

--- app/api/test_job.py
+++ app/api/test_job.py
@@ -4,15 +4,15 @@
 # -*- coding: UTF-8 -*-
 from flask import request, render_template, flash, redirect, url_for, abort, jsonify
 # from app.froms.project import ProjectForm
-from app.libs.test_email_inquiry import email_inquiry
+# from app.libs.test_email_inquiry import email_inquiry
 from app.models.project import Project
 from app.models.api_docs import ApiDocs
 from app.models import db
 from . import bp
 
-
-@bp.route('/test/email_inquiry', methods=['GET'])
-def test_email_inquiry():
-    """ 测试 """
-    email_inquiry()
-    return ("", 204)
+#
+# @bp.route('/test/email_inquiry', methods=['GET'])
+# def test_email_inquiry():
+#     """ 测试 """
+#     email_inquiry()
+#     return ("", 204)
--- app/libs/test_email_inquiry.py
+++ app/libs/test_email_inquiry.py
@@ -1,30 +1,30 @@
 # -*- coding: UTF-8 -*-
 import smtplib
-import yagmail
-from config import INQUIRY_EMAIL, INQUIRY_EMAIL_PW, INQUIRY_EMAIL_LIST
-
-
-def send_email(recipient, headline, contents):
-    yag = yagmail.SMTP(user=INQUIRY_EMAIL, password=INQUIRY_EMAIL_PW, host='smtp.126.com')
-    return yag.send(recipient, headline, contents)
-
-
-def email_inquiry():
-    import time
-    recipient = INQUIRY_EMAIL_LIST
-    time = time.time()
-    headline = f'全球赢-技术部-询盘{time}'
-    contents = f'全球赢-技术部-询盘测试{time}'
-
-    try:
-        send_email(recipient, headline, contents)
-        print('定时邮件询盘发送成功，请验收')
-    except smtplib.SMTPException:
-        print("Error: 无法发送邮件")
-
-
-def warning_email():
-    try:
-        send_email('gideon_tao@skytech.cn', '询盘发送定时任务异常报警', '询盘发送定时任务异常报警，请及时处理')
-    except smtplib.SMTPException:
-        print("Error: 无法发送邮件")
+# import yagmail
+# from config import INQUIRY_EMAIL, INQUIRY_EMAIL_PW, INQUIRY_EMAIL_LIST
+#
+#
+# def send_email(recipient, headline, contents):
+#     yag = yagmail.SMTP(user=INQUIRY_EMAIL, password=INQUIRY_EMAIL_PW, host='smtp.126.com')
+#     return yag.send(recipient, headline, contents)
+#
+#
+# def email_inquiry():
+#     import time
+#     recipient = INQUIRY_EMAIL_LIST
+#     time = time.time()
+#     headline = f'全球赢-技术部-询盘{time}'
+#     contents = f'全球赢-技术部-询盘测试{time}'
+#
+#     try:
+#         send_email(recipient, headline, contents)
+#         print('定时邮件询盘发送成功，请验收')
+#     except smtplib.SMTPException:
+#         print("Error: 无法发送邮件")
+#
+#
+# def warning_email():
+#     try:
+#         send_email('gideon_tao@skytech.cn', '询盘发送定时任务异常报警', '询盘发送定时任务异常报警，请及时处理')
+#     except smtplib.SMTPException:
+#         print("Error: 无法发送邮件")
