317712d29e65cff9c5d10a6d28ccc38b26dcf30b
Gideon0314 <923198284@qq.com>
2021-01-08 16:32:08 +0800

    V1.0

--- boot.sh
+++ boot.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
 
-while true; do
-    python manager.py db upgrade
-    if [[ "$?" == "0" ]]; then
-        break
-    fi
-    echo Failed to apply the migration to the database, retrying in 3 secs...
-    sleep 3
-done
 exec gunicorn -w 3 -b 0.0.0.0:5000 --access-logfile - --error-logfile - test_helper:app
