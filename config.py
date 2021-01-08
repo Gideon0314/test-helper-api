c5148d3f60b12624b43dd3880b5cd0ed5ad64a7e
Gideon0314 <923198284@qq.com>
2021-01-08 16:22:41 +0800

    V1.0

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 # class Config(object):
 #     pass
 
-HOSTNAME = '172.17.223.127'
+HOSTNAME = '123.56.117.178'
 PORT = '3306'
 DATABASE = 'test_helper'
 USERNAME = 'root'
