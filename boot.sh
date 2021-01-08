0507e0191f5965ad86080cef4cd0ef87486219ac
Gideon0314 <923198284@qq.com>
2021-01-08 14:41:34 +0800

    V1.0

--- boot.sh
+++ boot.sh
@@ -8,5 +8,4 @@ while true; do
     echo Failed to apply the migration to the database, retrying in 3 secs...
     sleep 3
 done
-flask deploy
 exec gunicorn -w 3 -b 0.0.0.0:5000 --access-logfile - --error-logfile - test_helper:app
