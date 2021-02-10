b650eca18910d3d8ece4d98f0d528cfdf732439a
Gideon0314 <923198284@qq.com>
2021-02-10 08:59:30 +0800

    V1.0

--- app/api/test_job.py
+++ app/api/test_job.py
@@ -1,94 +1,44 @@
 # -*- coding: UTF-8 -*-
-import os
-import subprocess
-from time import sleep
-from datetime import datetime
-from flask import render_template, jsonify
-from app.libs.zip_file import zip_compress
+from flask import jsonify
+from app.libs.run_case import web_test, api_cases_test
 from . import bp
+from concurrent.futures import ThreadPoolExecutor
 
 
-@bp.route('/api_test', methods=['GET'])
-def api_test():
-    """ 测试 """
-    project = 'httptest'
-    hrp = 'httprunner'
+executor = ThreadPoolExecutor(2)
 
-    now = datetime.now().strftime('%Y%m%d%H%M%S')
-    allure_result, allure_report = f"result_{now}", f"report_{now}"
 
-    project_path = os.path.abspath(os.path.dirname(os.getcwd()))
-    cases_path = project_path + f'\\{hrp}\\{project}\\testcases'
-    reports_path = project_path + f'\\{hrp}\\{project}\\reports'
-
-    cmd = [
-        # f"hrun {cases_path} --alluredir={reports_path}\\{allure_result}",
-        f"hrun {cases_path} --alluredir={allure_result}",
-        # f"allure serve {allure_result}",
-        f"allure generate {allure_result} -o {allure_report}",
-        # f"allure generate {reports_path}\\{allure_result}\ -o {reports_path}\\{allure_report}",
-    ]
-
-    for line in cmd:
-        ret = subprocess.run(line, shell=True, cwd=reports_path, timeout=60)
-        # if subp.returncode == 0:
-        # subp.wait(20)
-        if ret.returncode == 0:
-            sleep(10)
-            print("success:", ret)
-        else:
-            return jsonify(
-                {
-                    "status": 453,
-                }
-            )
-    zip_compress(f'{reports_path}\\{allure_report}', f'{reports_path}\\{allure_report}.zip')
+@bp.route('/web_jobs', methods=['GET'])
+def web_jobs():
+    """test"""
+    # executor.submit(api_test)
+    executor.submit(web_test)
     return jsonify(
         {
             "status": 200,
+            'msg': 'Two jobs was launched in background!'
         }
         )
 
 
-@bp.route('/web_test', methods=['GET'])
-def web_test():
-    """ 测试 """
-
-    hrp = 'web_test'
-    # project = 'httptest'
-
-    now = datetime.now().strftime('%Y%m%d%H%M%S%f')
-    allure_result = f"allure_result_{now}"
-    allure_report = f"allure_report_{now}"
-
-    project_path = os.path.abspath(os.path.dirname(os.getcwd()))
-    cases_path = project_path + f'\\{hrp}'
-    reports_path = project_path + f'\\{hrp}\\report'
-
-    cmd = [
-        f'cd C:\Test_Helper\web_test',
-        'run_test.bat',
-        # f'pytest --rootdir={cases_path} --reruns 2 --alluredir={reports_path}\\{allure_result}',
-        # f"allure generate {reports_path}/{allure_result} -c -o {reports_path}/{allure_report}",
-    ]
-
-    for line in cmd:
-        ret = subprocess.run(line, shell=True, timeout=20, cwd="C:\Test_Helper\web_test")
-        # if subp.returncode == 0:
-        # subp.wait(20)
-        sleep(10)
-        if ret.returncode == 0:
-            print("success:", ret)
-        else:
-            return jsonify(
-                {
-                    "status": 453,
-                }
-            )
-
+@bp.route('/api_jobs', methods=['GET'])
+def api_jobs():
+    """test"""
+    executor.submit(api_cases_test)
     return jsonify(
         {
             "status": 200,
+            'msg': 'Two jobs was launched in background!'
         }
         )
 
+
+# @bp.route('/api_jobs', methods=['GET'])
+# def api_job():
+#     executor.submit(api_test)
+#     return jsonify(
+#         {
+#             "status": 200,
+#             'msg': 'Two jobs was launched in background!'
+#         }
+#         )
--- app/libs/run_case.py
+++ app/libs/run_case.py
@@ -1,1 +1,93 @@
 # -*- coding: UTF-8 -*-
+import os
+import subprocess
+from datetime import datetime
+from time import sleep
+from app.libs.zip_file import zip_compress
+
+
+def api_case_test(case_name):
+    """ 测试 """
+
+    project = 'httptest'
+    hrp = 'httprunner'
+
+    now = datetime.now().strftime('%Y%m%d%H%M%S')
+    allure_result, allure_report = f"result_{now}", f"report_{now}"
+
+    project_path = os.path.abspath(os.path.dirname(os.getcwd()))
+    case_path = project_path + f'\\{hrp}\\{project}\\testcases\\{case_name}'
+    reports_path = project_path + f'\\{hrp}\\{project}\\reports'
+
+    cmd = [
+        f"hrun {case_path} --alluredir={allure_result}",
+        f"allure generate {allure_result} -o {allure_report}",
+    ]
+
+    for line in cmd:
+        ret = subprocess.run(line, shell=True, cwd=reports_path, timeout=60)
+        if ret.returncode == 0:
+            sleep(10)
+            print("success:", ret)
+        else:
+            print('ERROR')
+
+    zip_compress(f'{reports_path}\\{allure_report}', f'{reports_path}\\{allure_report}.zip')
+
+    print('success')
+
+
+def api_cases_test():
+    """ 测试 """
+
+    project = 'httptest'
+    hrp = 'httprunner'
+
+    now = datetime.now().strftime('%Y%m%d%H%M%S')
+    allure_result, allure_report = f"result_{now}", f"report_{now}"
+
+    project_path = os.path.abspath(os.path.dirname(os.getcwd()))
+    cases_path = project_path + f'\\{hrp}\\{project}\\testcases'
+    reports_path = project_path + f'\\{hrp}\\{project}\\reports'
+
+    cmd = [
+        # f"hrun {cases_path} --html=reports\pytest_report_{now}.html --self-contained-html",
+        f"hrun {cases_path} --html={now}.html --self-contained-html --alluredir={allure_result}",
+        f"allure generate {allure_result} -o {allure_report}",
+    ]
+
+    for line in cmd:
+        ret = subprocess.run(line, shell=True, cwd=reports_path, timeout=60)
+        if ret.returncode == 0:
+            sleep(10)
+            print("success:", ret)
+        else:
+            print('ERROR')
+
+    zip_compress(f'{reports_path}\\{allure_report}', f'{reports_path}\\{allure_report}.zip')
+
+    print('success')
+
+
+def web_test():
+    """ 测试 """
+    hrp = 'web_test'
+    # project = 'httptest'
+    now = datetime.now().strftime('%Y%m%d%H%M%S')
+    allure_result, allure_report = f"result_{now}", f"report_{now}"
+    project_path = os.path.abspath(os.path.dirname(os.getcwd()))
+
+    cmd = [
+        f'pytest --reruns 2 --html={now}.html --self-contained-html --alluredir=reports\\{allure_result} --clean-alluredir',
+        f'allure generate reports\\{allure_result} -c -o reports\\{allure_report}',
+        # r'allure open allure-report'
+    ]
+    for line in cmd:
+        ret = subprocess.run(line, shell=True, timeout=20, cwd=f"{project_path}\web_test")
+        sleep(10)
+        if ret.returncode == 0:
+            print("success:", ret)
+            '''记录运行结果'''
+        else:
+            print('error')
+    print('success')
