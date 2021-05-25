2bb5418cf609d1a50a566fbe7b2ac51460785bfa
Gideon0314 <923198284@qq.com>
2021-05-25 10:56:59 +0800

    新增构建记录接口

--- app/task/__init__.py
+++ app/task/__init__.py
@@ -1,3 +1,6 @@
+from apscheduler.schedulers.gevent import GeventScheduler
 from flask_apscheduler import APScheduler
 # 实例APScheduler定时任务
-scheduler = APScheduler()
+
+# scheduler = APScheduler()
+scheduler = GeventScheduler()
