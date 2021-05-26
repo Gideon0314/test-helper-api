fda10641d95158198138a3e9ddff1c43cbd43576
Gideon0314 <923198284@qq.com>
2021-05-26 19:47:27 +0800

    定时任务调试

--- test_helper.py
+++ test_helper.py
@@ -9,8 +9,8 @@ from app import create_app
 app = create_app(Config)
 
 
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', debug=app.config['DEBUG'], port=81, threaded=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=app.config['DEBUG'], port=81, threaded=True)
     # Manager.run(host='0.0.0.0', debug=app.config['DEBUG'], port=81, threaded=True)
     #单进程、单线程
     # processes = 1
