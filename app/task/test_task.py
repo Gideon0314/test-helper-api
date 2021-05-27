dc479ba9a996040d242f5c0b3db4c67f5be03b75
Gideon0314 <923198284@qq.com>
2021-05-27 11:44:48 +0800

    定时任务调试

--- app/task/test_task.py
+++ app/task/test_task.py
@@ -1,20 +1,21 @@
 import json
-from datetime import date
+from datetime import datetime
 import requests
 from app.extensions import scheduler
 
-@scheduler.task(
-    "interval",
-    id="1",
-    seconds=10,
-    max_instances=1,
-)
+
+# @scheduler.task(
+#     "interval",
+#     id="1",
+#     seconds=10,
+#     max_instances=1,
+# )
 def my_job():
     url = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=cb47d771-e6fd-49e4-9df7-39ff1eb634e2'
     data = {
         "msgtype": "markdown",
         "markdown": {
-            "content":f"{date.today()}定时任务测试\n"
+            "content":f"{datetime.now()}定时任务测试\n"
             }
         }
     r = requests.post(url, json.dumps(data))
@@ -22,19 +23,17 @@ def my_job():
         print('消息发送成功')
 
 
-
-
-@scheduler.task(
-    "interval",
-    id="2",
-    seconds=10,
-    max_instances=1,
-)
-def task1():
-    """Sample task 1.
-    Added when app starts.
-    """
-    print("running task 1!")  # noqa: T001
-    # oh, do you need something from config?
-    with scheduler.app.app_context():
-        print(scheduler.app.config)  # noqa: T001
+# @scheduler.task(
+#     "interval",
+#     id="2",
+#     seconds=10,
+#     max_instances=1,
+# )
+# def task1():
+#     """Sample task 1.
+#     Added when app starts.
+#     """
+#     print("running task 1!")  # noqa: T001
+#     # oh, do you need something from config?
+#     with scheduler.app.app_context():
+#         print(scheduler.app.config)  # noqa: T001
